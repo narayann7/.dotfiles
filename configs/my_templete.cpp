7e99a1124c6e779e100d3924d480158e479ee653
narayann7 <laxminarayanreddy432@gmail.com>
2021-10-16 23:54:33 +0530

    added config

---
+++ configs/my_templete.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//=======================
+#define ll long long
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+//=======================
+
+void solve() {
+  int i, j, n, m,c;
+  string str;
+  
+
+}
+
+int main() {
+
+    int t = 1;
+    cin >> t;
+    while(t--) {
+      solve();
+    }
+
+    return 0;
+}
+
