4ce6c343798af4460ad25e91b25ebb057f114bb6
narayann7 <laxminarayanreddy432@gmail.com>
2022-02-05 21:41:29 +0700

    'testing_commit'

--- useful scripts/cmd_scripts/alias.cmd
+++ useful scripts/cmd_scripts/alias.cmd
@@ -4,3 +4,5 @@
 doskey c=cls
 doskey pwd=cd
 doskey ls=dir
+doskey pushall=python C:\Users\narayann7\scripts\pushAllGitHubRepo.py 0
+doskey pushallm=python C:\Users\narayann7\scripts\pushAllGitHubRepo.py 1
