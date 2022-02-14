45fd4c4bb2f40b915d5ad0d3e5397f2b40dc3841
narayann7 <laxminarayanreddy432@gmail.com>
2022-02-14 13:46:17 +0700

    'testing_commit'

--- useful_scripts/live_pc_scripts/scrcpy.py
+++ useful_scripts/live_pc_scripts/scrcpy.py
@@ -1,6 +1,7 @@
-import os
+import os 
+os.system("adb tcpip 5555")
+os.system("adb connect 192.168.0.103:5555")
+# os.system("scrcpy --bit-rate 2M --max-size 800")
+os.system("scrcpy -b2M -m800")
 
-os.system('C:\\Users\\narayann7\\AppData\\Roaming\\Microsoft\\Windows\\"Start Menu"\\Programs\\ScrCpy\\ScrCpy.lnk')
-
-
-# it cant run thought kde so , python script helps to run.
+# to switch back $ adb usb
