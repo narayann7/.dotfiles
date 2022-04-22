89ee1797a932785ff008ddc9369b751b00f743f2
Narayan <laxminarayanreddy432@gmail.com>
2022-04-22 19:19:22 +0700

    'added-image-to-pdf-with-base64'

---
+++ useful_scripts/python_scripts/images_to_pdf.py
@@ -0,0 +1,66 @@
+# install these librarys
+# pip install pillow
+# pip install img2pdf
+# pip install pikepdf
+
+
+import os
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+import img2pdf
+from pikepdf import Pdf
+import pikepdf
+import base64
+
+def string_to_base64(string):
+    return base64.b64encode(string.encode('utf-8')).decode('utf-8')
+
+def put_password(file_path,folder_name):
+     pdf = Pdf.open(file_path)  
+     output=folder_name+".xml"
+     password=string_to_base64(output)
+     pdf.save(output, encryption=pikepdf.Encryption(owner=password, user=password, R=4)) 
+     pdf.close()
+
+
+def image_to_pdf(path):
+ images=  [i for i in os.listdir(path) if i.endswith(".jpg")]
+ print(images)
+ os.chdir(path)
+ with open("output.pdf", "wb") as f:
+    f.write(img2pdf.convert(images),)
+
+
+def compress_image(file_path):
+ img = PIL.Image.open(file_path) 
+ myHeight, mywidth = img.size
+ size=os.stat(file_path).st_size
+ 
+ if size>=900000:
+   img=img.resize((myHeight, mywidth), PIL.Image.ANTIALIAS) 
+   save_path = file_path.split('.')[0] + '_.jpg'
+   img.save(save_path)
+   os.remove(file_path)
+
+path=os.getcwd()
+print(path)
+
+os.chdir(path)
+for file in os.listdir(path):
+    if file.endswith(".jpg"):
+        compress_image(path+'\\'+file)
+
+folder_name=path.split('\\')[-1]   
+
+full_path=path+'\\output.pdf'
+
+image_to_pdf(path)
+
+# delete all images
+for file in os.listdir(path):
+    if  file.endswith(".jpg") and file.endswith("_.jpeg")  :
+        os.remove(file) 
+            
+put_password(full_path,folder_name)   
+os.remove(full_path) 
