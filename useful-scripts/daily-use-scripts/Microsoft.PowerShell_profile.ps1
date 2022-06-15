d55fd98e954647b486d9f3544b386cdb2f1a2bb1
Narayan <laxminarayanreddy432@gmail.com>
2022-06-15 22:06:07 +0700

    'commit_from_script'

--- useful-scripts/daily-use-scripts/Microsoft.PowerShell_profile.ps1
+++ useful-scripts/daily-use-scripts/Microsoft.PowerShell_profile.ps1
@@ -116,11 +116,11 @@ Function gcp {
 }
 
 Function pushall {
-    $command = "python C:\Users\narayann7\Documents\love\GitHubRepos\scripts-n-files\useful_scripts\daily-use-scripts\push_all_gitHub_repo.py 0"
+    $command = "python C:\Users\narayann7\Documents\love\GitHubRepos\scripts-n-files\useful-scripts\daily-use-scripts\pushAllGitHubRepo.py 0"
     Invoke-Expression $command
 }
 Function pushallm {
-    $command = "python C:\Users\narayann7\Documents\love\GitHubRepos\scripts-n-files\useful_scripts\daily-use-scripts\push_all_gitHub_repo.py 1"
+    $command = "python C:\Users\narayann7\Documents\love\GitHubRepos\scripts-n-files\useful-scripts\daily-use-scripts\pushAllGitHubRepo.py 1"
     Invoke-Expression $command
 }
 
@@ -145,12 +145,16 @@ Function op7 {
         [string[]]
         $port
     )
-    $path = "python C:\Users\narayann7\Documents\love\GitHubRepos\scripts-n-files\useful_scripts\push_all_gitHub_repo.py\adb_connect_wireless.py $port"
+    $path = "python C:\Users\narayann7\Documents\love\GitHubRepos\scripts-n-files\useful-scripts\daily-use-scripts\adb_connect_wireless.py $port"
     Invoke-Expression $path
 }
 
 function sudo {
     Start-Process @args -verb runas
+}
+
+function envop{
+    sysdm.cpl
 }
 
 Set-PSReadLineOption -PredictionSource History
--- useful-scripts/daily-use-scripts/adb_connect_wireless.py
+++ useful-scripts/daily-use-scripts/adb_connect_wireless.py
@@ -2,4 +2,4 @@ import os
 import sys
 port=sys.argv[1]
 os.chdir("C:\\Users\\narayann7\\AppData\Local\\Android\\Sdk\\platform-tools")
-os.system(f"adb connect 192.168.0.102:{port}")
+os.system(f"adb connect 192.168.0.103:{port}")
