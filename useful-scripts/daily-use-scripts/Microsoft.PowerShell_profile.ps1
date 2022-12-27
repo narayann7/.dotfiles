e3df1c937dd353f60098fe1d50963067331d2172
Narayan <laxminarayanreddy432@gmail.com>
2022-12-27 10:07:49 +0530

    'commit_from_script'

--- useful-scripts/daily-use-scripts/Microsoft.PowerShell_profile.ps1
+++ useful-scripts/daily-use-scripts/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,5 @@
-
-
 Clear-Host
+#welcome script
 #python #C:\Users\narayann7\Documents\GitHubRepos\scripts-n-files\useful-scripts\daily-use-scripts\welcome.py
 
 
@@ -8,10 +7,9 @@ Clear-Host
 #theme shell 
 Import-Module posh-git
 Import-Module oh-my-posh
-
 Set-PoshPrompt darkblood
 
-
+#alias
 set-alias -name c -value clear
 set-alias -name ec -value echo
 set-alias -name dk -value docker
@@ -19,14 +17,44 @@ set-alias -name l -value ls
 set-alias -name pp -value python
 set-alias -name run -value  Invoke-Expression
 
-
+#function alias
 Function e { exit }
 Function pg { flutter pub get }
-Function ep { sub $Profile }
 
+
+
+#dir paths
+Function ghr { Set-Location C:\Users\narayann7\Documents\GitHub }
+Function hm { Set-Location ~ }
+Function down { Set-Location "~/Downloads" }
+Function doc { Set-Location "~/Documents" }
+Function test { Set-Location "C:\Users\narayann7\testing" }
+Function dump { Set-Location "C:\Users\narayann7\testing\dump" }
+Function scripts { Set-Location "C:\Users\narayann7\Documents\GitHubRepos\scripts-n-files" }
+
+#to create temp folder 
+Function tdump { 
+   Param(
+      [Parameter(Mandatory = $true,
+         ValueFromPipeline = $true)]
+      [string[]]
+      $folder_name
+   ) 
+   Set-Location "C:\Users\narayann7\testing\dump"
+
+   mkdir $folder_name
+   Set-Location "C:\Users\narayann7\testing\dump\$folder_name"
+}
+
+
+#file paths
+Function epvc { code $Profile }
+Function ep { sub $Profile }
 Function his { sub C:\Users\narayann7\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt }
 
 
+
+#genral alias
 Function touch {
    Param(
       [Parameter(Mandatory = $true,
@@ -34,7 +62,6 @@ Function touch {
       [string[]]
       $file_name
    ) New-Item $file_name
-
 
 }
 
@@ -48,17 +75,6 @@ Function file {
    $command = "explorer $file_name"
    Invoke-Expression $command
 }
-
-Function epvc { code $Profile }
-Function hm { Set-Location ~ }
-Function down { Set-Location "~/Downloads" }
-Function doc { Set-Location "~/Documents" }
-Function dump { Set-Location "C:\Users\narayann7\Documents\GitHubRepos\dump" }
-Function dumpl { Set-Location "C:\Users\narayann7\dumpl" }
-Function scripts { Set-Location "C:\Users\narayann7\Documents\GitHubRepos\scripts-n-files" }
-Function test { Set-Location "C:\Users\narayann7\Documents\GitHubRepos\dump\testing" }
-Function testl { Set-Location "C:\Users\narayann7\testing" }
-Function gr { Set-Location "C:\Users\narayann7\Documents\GitHubRepos" }
 
 Function sub {
    Param(
@@ -78,6 +94,8 @@ Function sub {
    }
 
 }
+
+#set of git commands
 Function ghinit {
    Param(
       [Parameter(Mandatory = $true,
@@ -101,20 +119,6 @@ Function ghinit {
    git remote add origin https://github.com/narayann7/$repo_name.git
    git push -u origin main
 
-
-
-}
-
-function ffile($name) {
-   Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
-      $place_path = $_.directory
-      Write-Output "${place_path}\${_}"
-   }
-}
-function unzip ($file) {
-   Write-Output("Extracting", $file, "to", $pwd)
-   $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
-   Expand-Archive -Path $fullFile -DestinationPath $pwd
 }
 Function gac {
    Param(
@@ -126,6 +130,20 @@ Function gac {
    git add .
    git commit -m "$message"
 }
+
+#search file in current dir
+function ffile($name) {
+   Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+      $place_path = $_.directory
+      Write-Output "${place_path}\${_}"
+   }
+}
+function unzip ($file) {
+   Write-Output("Extracting", $file, "to", $pwd)
+   $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
+   Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+
 Function op {
    Param(
       [Parameter(Mandatory = $true,
@@ -208,7 +226,7 @@ Function whereis {
       [string[]]
       $command
    )
-(gcm $command).Path
+(Get-Command $command).Path
 }
 
 Function idraw {
@@ -233,16 +251,7 @@ Function edraw {
    New-Item $filename
 }
 
+
+#powershell configs
 Set-PSReadLineOption -PredictionViewStyle ListView
-
 Set-PSReadLineOption -PredictionSource History
-
-
-
-
-
-
-
-
-
-
