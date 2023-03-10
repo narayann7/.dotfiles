3e4c7bf2428c788f1d9c373ea929a03d18cfb3b8
Narayan <laxminarayanreddy432@gmail.com>
2023-03-10 12:33:54 +0530

    'commit_from_script'

--- useful-scripts/daily-use-scripts/scrcpy.py
+++ useful-scripts/daily-use-scripts/scrcpy.py
@@ -1,6 +1,6 @@
 import os 
 os.system("adb tcpip 5555")
-os.system("adb connect 192.168.0.103:5555")
+os.system("adb connect 192.168.1.116:5555")
 # os.system("scrcpy --bit-rate 2M --max-size 800")
 os.system("scrcpy -b2M -m800")
 
